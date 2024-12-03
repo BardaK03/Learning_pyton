a8d60d801d6b3eb80f06b6beb38c0cf3c1e446c5
Darius <bardadarius6@gmail.com>
2024-12-03 23:06:12 +0200

    ex15

--- test1.py
+++ test1.py
@@ -1,2 +1,11 @@
-height = int(input("how tall are you"))
-credits = int(input("how many credits do you have"))
+height = int(input("how tall(cm) are you?:"))
+credits = int(input("how many credits do you have?:"))
+
+if height >=137 and credits >=10:
+    print("Enjoy the ride!")
+elif height <137 and credits >=10:
+    print("You are not tall enough to ride.")
+elif height >=137 and credits <10:
+    print("You don't have enough credits.")
+else:
+    print("You don t meet any requirements.")
